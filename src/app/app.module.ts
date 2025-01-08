2e4ad278524c475fe5b011b1497cac0b41e1be01
Jesus Urbina <jesusrug2002@gmail.com>
2025-01-08 02:27:11 -0400

    feat: database confing and files config

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './services/app.service';
+import { DatabaseModule } from '../database/database.module';
+import { AppController } from './controllers/app.controller';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/database/database.provider.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'admin',
+        password: 'admin123',
+        database: 'file_management_db',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      });
+      return dataSource.initialize();
+    },
+  },
+];
