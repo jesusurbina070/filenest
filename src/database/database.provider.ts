a2a6e41aeaefe11abe61bd1eefad154fbb3d7f33
Jesus Urbina <jesusrug2002@gmail.com>
2025-01-28 23:47:16 -0400

    conectamos con la base de datos en rds

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -6,13 +6,12 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'admin',
-        password: 'admin123',
-        database: 'file_management_db',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
       });
       return dataSource.initialize();
     },
